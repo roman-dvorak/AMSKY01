cf8d57397e5b3a260450e6f3719c00f8d83c478e
Roman Dvorak <romandvorak@mlab.cz>
2025-08-24 16:24:45 +0200

    setup scripts

---
+++ version.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Version management for AMSKY01 package
+"""
+
+__version__ = "1.0.0"
+
+def get_version():
+    """Get the current version"""
+    return __version__
+
+if __name__ == "__main__":
+    print(get_version())
